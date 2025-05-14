d004b9d73f3da6eff19300796ece2f8d1c101f1a
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2025-05-14 15:46:27 +0600

    fix responsive in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,13 +6,13 @@ const HeroSection = () => {
   return (
     <div className="min-h-screen w-full bg-[url('/hero-banner.jpg')] bg-cover bg-center">
       <div className="relative flex min-h-screen flex-col items-center justify-center space-y-5 bg-gradient-to-b from-black/70 via-black/80 to-black/90 px-2 text-center md:px-0">
-        <div className="absolute top-1 -left-5 flex -rotate-45 transform flex-col items-center gap-2 border-b-2 border-green-400 px-2 pt-5 pb-1 md:top-7 md:left-0 md:pt-0">
+        <div className="absolute top-1 -left-5 flex -rotate-45 transform flex-col items-center gap-2 border-b-2 border-green-400 px-2 pt-8 pb-1 md:top-7 md:left-0 md:pt-0">
           <FaCheckCircle className="text-xl text-green-400" />
           <p className="text-sm font-bold tracking-wide text-green-400 uppercase md:text-lg">
             Available
           </p>
         </div>
-        <p className="text-2xl">Complete twenty Deals to claim</p>
+        <p className="text-xl md:text-2xl">Complete twenty Deals to claim</p>
         <h1 className="text-5xl font-bold md:text-7xl">$750 To Your PayPal</h1>
         <Image
           src={paypalCard}
