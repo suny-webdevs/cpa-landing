2b2d2da260296a6793f0eabb9d2d42e1d6aa644b
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2025-05-17 21:32:25 +0600

    fix env

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,28 @@
 import { decodeToken } from "@/utils/decodeToken";
+import { Metadata } from "next";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "CPA | Admin Dashboard",
+};
 
 const DashboardPage = async () => {
   const tokenStore = await cookies();
   const token = tokenStore.get("token")?.value;
+
+  if (!token) {
+    redirect("/");
+  }
+
   const email = decodeToken(token as string)?.email;
 
-  const res = await fetch(`${process.env.NEXT_API_URL}/auth/user/${email}`, {
-    headers: { Authorization: `${token}` },
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/user/${email}`,
+    {
+      headers: { Authorization: `${token}` },
+    },
+  );
   const userData = await res.json();
   const user = userData?.data;
 
@@ -18,6 +32,8 @@ const DashboardPage = async () => {
       <p>Name : {user?.name}</p>
       <p>Email : {user?.email}</p>
       <p>Role : {user?.role}</p>
+
+      <button className="cursor-pointer">Logout</button>
     </div>
   );
 };
--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -15,7 +15,7 @@ const LoginForm = () => {
     const password = form.password.value;
 
     try {
-      const res = await fetch(`${process.env.NEXT_API_URL}/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
